b8e863fd03a8ffff3c1e1f06634c355255ca0866
colvert <morgan.richomme@gmail.com>
2015-09-28 13:41:26 +0200

    ajout tripalium dans la liste des partenaires

--- www/data.php
+++ www/data.php
@@ -4,22 +4,22 @@
 
 $data_partenaires = array (
    array(
-      "Fablab Lannion",
-      "img/fablab.jpg",
-      "http://fablab-lannion.org",
-      "  Atelier participatif ouvert, le fablab de Lannion accueille petits et grands. Un objet à réparer, une idée à transformer, un besoin de support dans un domaine technique, le fablab de Lannion vous ouvre ses portes. Venez découvrir les imprimantes 3D, les logiciels Libres, les mini ordinateurs et autres robots."
-   ),
-   array(
       "Code d'Armor",
       "img/codedarmor.png",
       "http://www.codedarmor.fr/",
       "  Code d'Armor a pour but de construire une communauté de développeurs sur la région de Lannion. Nous organisons des évènements autour du monde du développement informatique. Passionnés, Intéressés, Motivés, Interloqués, vous êtes tous les bienvenus !"
    ),
+    array(
+      "ENSSAT",
+      "img/ENSSAT.png",
+      "http://www.enssat.fr/",
+      "L’Enssat (École Nationale Supérieure des Sciences Appliquées et de Technologie) est une école publique d’ingénieurs et un pôle de recherche dans le domaine des télécoms et des technologies émergentes. Grâce à ses équipes d’enseignement et de recherche en synergie avec le monde industriel, l’École dote ses élèves ingénieurs de compétences et de savoir-faire à la pointe des technologies pour un avenir assuré dans le développement de projets innovants."
+   ),
    array(
-      "PATG",
-      "img/patg.png",
-      "https://sites.google.com/site/patglannion2010/",
-      "Promouvoir et Agir en Trégor Goélo (PATG) est une association axée sur l'aide aux particuliers et aux très petites entreprises du Trégor Goélo. PATG tente de réduire la fracture sociale et la fracture technique en visant la reprise des relations humaines et la réinsertion sociale. L'informatique est utilisée comme support parce que c'est un élément qui permet facilement les rapprochements humains, et apporte un complément de connaissances utile pour l'emploi et la vie courante"
+      "Fablab Lannion",
+      "img/fablab.jpg",
+      "http://fablab-lannion.org",
+      "  Atelier participatif ouvert, le fablab de Lannion accueille petits et grands. Un objet à réparer, une idée à transformer, un besoin de support dans un domaine technique, le fablab de Lannion vous ouvre ses portes. Venez découvrir les imprimantes 3D, les logiciels Libres, les mini ordinateurs et autres robots."
    ),
     array(
       "Les petits débrouillards",
@@ -33,12 +33,18 @@ $data_partenaires = array (
       "http://www.lycee-ledantec.ac-rennes.fr/",
       "Lycée public de Lannion, Félix le Dantec s'implique depuis plusieurs années dans le logiciel libre et depuis 3 ans organise chaque année une 'InstallParty & Initiation à GNU/Linux'.Nombre d'élèves (de STI et SSI) et étudiants (BTS/Licences Pro) ont été formé à l'usage et au développement sur le logiciel libre et l'openhardware."
    ),
-    array(
-      "ENSSAT",
-      "img/ENSSAT.png",
-      "http://www.enssat.fr/",
-      "L’Enssat (École Nationale Supérieure des Sciences Appliquées et de Technologie) est une école publique d’ingénieurs et un pôle de recherche dans le domaine des télécoms et des technologies émergentes. Grâce à ses équipes d’enseignement et de recherche en synergie avec le monde industriel, l’École dote ses élèves ingénieurs de compétences et de savoir-faire à la pointe des technologies pour un avenir assuré dans le développement de projets innovants."
+   array(
+      "PATG",
+      "img/patg.png",
+      "https://sites.google.com/site/patglannion2010/",
+      "Promouvoir et Agir en Trégor Goélo (PATG) est une association axée sur l'aide aux particuliers et aux très petites entreprises du Trégor Goélo. PATG tente de réduire la fracture sociale et la fracture technique en visant la reprise des relations humaines et la réinsertion sociale. L'informatique est utilisée comme support parce que c'est un élément qui permet facilement les rapprochements humains, et apporte un complément de connaissances utile pour l'emploi et la vie courante"
    ),
+    array(
+      "Tripalium",
+      "img/tripalium.jpeg",
+      "http://www.tripalium.org/",
+      "Impulsée en 2007 par la volonté de développer les stages d’auto-construction, l’association Tripalium s’est transformée en un réseau d’acteur-rices. Sans salarié ni subvention, sa vitalité repose sur la dynamique des stages : les anciens stagiaires sont invités à devenir formateurs, puis à organiser des stages...Tripalium échange également à l’international avec des organisations similaires."
+   ),   
 );
 
 Flight::set('data_partenaires', $data_partenaires);
