c6b2c02fed74dc1bb433d9f41527e083c6e7749b
jerome-labidurie <jerome@labidurie.fr>
2015-12-03 20:42:41 +0100

    Add goole analytics id

--- www/views/layout.php
+++ www/views/layout.php
@@ -32,6 +32,18 @@
 
 <body>
 
+<!-- google analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70992078-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
         <div class="container topnav">
@@ -62,7 +74,7 @@
                     </li>
                     <li>
                         <a href="/sponsors">Sponsors</a>
-                    </li>                    
+                    </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
