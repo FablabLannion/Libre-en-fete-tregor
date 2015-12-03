69f444e0e379044adcb3c7110fbbad6f448a44ae
jerome-labidurie <jerome@labidurie.fr>
2015-12-03 21:06:40 +0100

    Mentions légales

--- www/views/credits.php
+++ www/views/credits.php
@@ -18,7 +18,7 @@
    <li>
       <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Yalta_Conference_%28Churchill%2C_Roosevelt%2C_Stalin%29_%28B%26W%29.jpg/350px-Yalta_Conference_%28Churchill%2C_Roosevelt%2C_Stalin%29_%28B%26W%29.jpg"> Yalta conference</a>,
       Public Domain,
-      Modification avec gimp par Morgan Richomme, <a href="https://creativecommons.org/licenses/by/2.0/">CC:BY</a>  
+      Modification avec gimp par Morgan Richomme, <a href="https://creativecommons.org/licenses/by/2.0/">CC:BY</a>
    </li>
    <li>
 	   <a href="https://en.wikipedia.org/wiki/Liberty_Leading_the_People#/media/File:Eug%C3%A8ne_Delacroix_-_La_libert%C3%A9_guidant_le_peuple.jpg">La Liberté guidant le peule d'Eugène Delacoix</a>,
@@ -48,15 +48,15 @@
    <li>
 	   <a href="https://openclipart.org/detail/226095/professional-people-silhouette">Professional People Silhouette by <a href="https://openclipart.org/user-detail/GDJ">GDJ</a></a>,
 	   Public Domain (OpenClipArt)
-   </li> 
+   </li>
    <li>
 	   <a href="https://openclipart.org/detail/177797/badges">Badges by <a href="https://openclipart.org/user-detail/tatica">tatica</a></a>,
 	   Public Domain (OpenClipArt)
-   </li>    
+   </li>
    <li>
 	   <a href="https://openclipart.org/detail/60109/award-symbol">award symbol by <a href="https://openclipart.org/user-detail/sheikh_tuhin"></a>Sheikh Tuhin</a>,
 	   Public Domain (OpenClipArt)
-   </li>    
+   </li>
    <li>
 	   <a href="https://openclipart.org/detail/189359/penguins-just-love-openclipart">Penguins just love OpenClipart! by <a href="https://openclipart.org/user-detail/Moini"></a>Moini</a>,
 	   Public Domain (OpenClipArt)
@@ -81,4 +81,27 @@
    </li>
 </ul>
 
-</div></div></div>
+<h2>Mentions légales</h2>
+
+<ul>
+    <li>Hébergement<br/>
+        OVH<br/>
+        2 rue Kellermann<br/>
+        59100 Roubaix - France.<br/>
+        <a href="https://www.ovh.com/">ovh.com</a>
+    </li>
+</ul>
+
+</div>
+</div>
+
+<div class="col-lg-12">
+    <h3>Google Analytics</h3>
+    <small>
+        Ce site utilise Google Analytics, un service d'analyse de site internet fourni par Google Inc. (« Google »). Google Analytics utilise des cookies, qui sont des fichiers texte placés sur votre ordinateur, pour aider le site internet à analyser l'utilisation du site par ses utilisateurs. Les données générées par les cookies concernant votre utilisation du site (y compris votre adresse IP) seront transmises et stockées par Google sur des serveurs situés aux Etats-Unis. Google utilisera cette information dans le but d'évaluer votre utilisation du site, de compiler des rapports sur l'activité du site à destination de son éditeur et de fournir d'autres services relatifs à l'activité du site et à l'utilisation d'Internet.
+        <br/>
+        Google est susceptible de communiquer ces données à des tiers en cas d'obligation légale ou lorsque ces tiers traitent ces données pour le compte de Google, y compris notamment l'éditeur de ce site. Google ne recoupera pas votre adresse IP avec toute autre donnée détenue par Google. Vous pouvez désactiver l'utilisation de cookies en sélectionnant les paramètres appropriés de votre navigateur. Cependant, une telle désactivation pourrait empêcher l'utilisation de certaines fonctionnalités de ce site. En utilisant ce site internet, vous consentez expressément au traitement de vos données nominatives par Google dans les conditions et pour les finalités décrites ci-dessus.
+    </small>
+</div>
+
+</div>
