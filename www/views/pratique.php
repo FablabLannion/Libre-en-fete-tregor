9db6e8323f5f99d43a640d6added3c04faf2fee6
jerome-labidurie <jerome@labidurie.fr>
2016-03-10 18:32:40 +0100

    Link to dossier de presse

--- www/views/pratique.php
+++ www/views/pratique.php
@@ -37,16 +37,15 @@
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Horaires</h2>
+                    <h2 class="section-heading">Presse</h2>
                     <p class="lead">
-                    Samedi 26 Mars 2016 de 10h à 18h <br/>
-                    Entrée libre et gratuite <br/>
-                    Voir le <a href="/programme">programme</a> pour plus de détails
+                    <ul>
+                    <li> <a href="img/dossier_presse.pdf">Dossier de presse</a> </li>
+                    </ul>
                     </p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                <!-- https://www.flickr.com/photos/denisbourez/18781756013 -->
-                    <img class="img-responsive" src="img/montre.jpg" alt="">
+                    <a href="img/Affiche2.png"><img class="img-responsive" src="img/Affiche2_small.png" alt=""></a>
                 </div>
             </div>
 
@@ -55,9 +54,39 @@
 
     </div>
     <!-- /.content-section-b -->
-    
-    
+
       <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Comment s'y rendre ?</h2>
+                    <p  class="lead">
+                    <a href="http://www.openstreetmap.org/directions?engine=osrm_car&route=48.72699%2C-3.45662%3B48.73128%2C-3.45346#map=17/48.72927/-3.45452&layers=T">Depuis la gare routière</a><br/>
+                    <a href="http://www.openstreetmap.org/directions?engine=osrm_car&route=48.72745%2C-3.46037%3B48.73128%2C-3.45346#map=17/48.72960/-3.45676&layers=T">Depuis la gare SNCF</a><br/>
+                    <a href="http://www.openstreetmap.org/directions?engine=osrm_car&route=48.7559%2C-3.4719%3B48.7313%2C-3.4535#map=14/48.7434/-3.4557&layers=T">Depuis l'aéroport</a><br>
+                    Plus d'information sur le <a href="http://www.ville-lannion.fr/page_decouvrirlannion_veniretsedepl_menu1.html">site de la ville de Lannion</a>
+                    </p>
+                </div>
+                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                carte interactive
+                    <iframe width="100%" height="300px" frameBorder="0" src="http://umap.openstreetmap.fr/en/map/untitled-map_54949?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false#16/48.7313/-3.4547">
+                    </iframe>
+                    <p>
+                      <a href="http://umap.openstreetmap.fr/en/map/untitled-map_54949">Carte en plein écran</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.content-section-a -->
+
+      <div class="content-section-b">
 
         <div class="container">
             <div class="row">
@@ -105,5 +134,5 @@ Ce Code de Conduite est adapté du <a href="http://2014.jsconf.eu/code-of-conduc
         </div>
         <!-- /.container -->
     </div>
-    <!-- /.content-section-a -->
-    
+    <!-- /.content-section-b -->
+
