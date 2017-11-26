4dbae78d3ac7e7dbbb7068e5caec079ab8c6ee2c
FabTangi <tangilavanant@gmail.com>
2017-11-26 21:21:41 +0100

    accueil_br.php
    1ere propal de traduction de l'accueil (a terminer et corrigé)

---
+++ www/views/accueil_br.php
@@ -0,0 +1,58 @@
+
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Distro Gouel ar Frank e Treger</h1>
+                        <h3>D'ar Sul 25 a viz Meurzh - Sal an Ursulinezed - 10h-18h</h3>
+                        <hr class="intro-divider">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="https://twitter.com/librenfetetrgor" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/Libre-en-F%C3%AAte-Tr%C3%A9gor-970420503044317/timeline" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+
+
+        <div class="content-section-a">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">À propos</h2>
+                    <p class="lead">
+                    18vet deizh ar bloazh Gouel ar Frank, savet ha kenurziet gant an April, a vo lidet e 2018. Degouezhadennoù dizoloein bed ar frank a vo kiniget a dreuz ar Frans, en ur stumm laouenek ha dudius.</p>
+                    <p>
+                    Ur strollad kevredigiezhioù a vro Dreger a ginnigo asamblez stalieroù tro-dro d'ar meziantoù frank ar Sul 25 a viz Meurzh 2018 sal an Ursulinezed e Lannuon
+                    </p>
+                </div>
+
+                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                    <p>
+                    Souezus e c'hell seblantout liamman ar gêr frank gant ar gêr meziant, koulskoude eman ar meziant frank e peb lec'h.  War o urzhiataer eman, heptan rouedad ebet, gellout a ra rein lans en dro d'o'ch urzhiataer gozh. Kinning a ra ivez meziantoù evit diskouezh be framman filmoù, sevel testennoù, renkan poltriji...
+                    </p>
+                    <p>
+                    Disanav e chom, dre ma n'eo ket sikouret gant embregerezioù vras. Ar meziant frank a zegas talvoudegezhioù ken-sikour ha kenlabouret a chell bezan implijet gant pep hini. Il représente ainsi un patrimoine immatériel façonnant le quotidien numérique de chacun.
+                    A travers des animations gratuites, les Trégorrois pourront découvrir ou redécouvrir la richesse du logiciel libre.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
