701b43dc6c686fef33dbe6fcc84782d510b8061f
jerome-labidurie <jerome@labidurie.fr>
2020-03-08 16:01:54 +0100

    Fix install party

--- www/data.php
+++ www/data.php
@@ -178,7 +178,7 @@ $data_programme = array (
   array("Découvrir",
      "img/4979030420_77b090328d_z.jpg",
      "",
-     "<bInstall Party</b><br/>
+     "<b>Install Party</b><br/>
      Votre ordinateur est lent, infesté de virus ? Vous avez toujours voulu essayer Linux ? Votre vieux scanner ne fonctionne plus sous Windows ?<br/>
      Venez avec votre compagnon numérique et sautez le pas ! Les étudiants du Lycée Le Dantec, et des membres du Fablab sont la pour vous guider dans l'installation de GNU/Linux qui lui donnera peut-être une nouvelle vie.<br/>
      <i>N'oubliez pas de sauvegarder vos données avant !</i>
